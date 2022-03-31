3e3d1604c0672e6a9ccfca24ba999f992bbd8291
Tien Dao <tiendao5240@gmail.com>
2022-03-30 21:19:35 -0400

    Task 9: Creating a performant lazy list

--- app/src/main/java/tdao/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/tdao/example/basicscodelab/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -58,9 +60,9 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
 
 
 @Composable
-private fun Greetings(names: List<String> = listOf("Compose", "Tien")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+private fun Greetings(names: List<String> = List(1000) { "Tien $it" } ) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
